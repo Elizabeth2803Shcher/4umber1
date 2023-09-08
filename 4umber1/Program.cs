053efae2be860558e054aaa03b7b033072790423
Elizabeth2803 <elizaart2801@gmail.com>
2023-09-08 19:49:21 +0200

    Добавьте файлы проекта.

---
+++ 4umber1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Дано четырехзначное число. Найти: а) сумму его цифр; б) произведение его цифр.");
+Console.Write("Введите n:");
+int n = int.Parse(Console.ReadLine());
+int d = n % 10;
+int c = n % 100 / 10;
+int b = n % 1000 / 100;
+int a = n / 1000;
+
+Console.Write("а) Сумма цифр введенного числа равна:");
+Console.WriteLine(a + b + c + d);
+Console.Write("б) Произведение цифр введенного числа равна:");
+Console.WriteLine(a * b * c * d);
